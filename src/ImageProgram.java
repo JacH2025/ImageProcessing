d09e436fe4338b8b3ca77ce7638c8fecc3b7772a
SymNC <abdul-hadi.s@northeastern.edu>
2022-11-22 16:37:07 -0500

    updated ImageProgram for hw6

--- src/ImageProgram.java
+++ src/ImageProgram.java
@@ -2,9 +2,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
+import java.util.Locale;
 
-import controller.ImageController;
 import controller.ImageControllerImpl;
+import controller.ImageControllerView;
+import view.ImageView;
+import view.ImageViewFrame;
 
 /**
  * Image Program created as the entry point to the Image Processing program. Allows you to use
@@ -12,30 +15,69 @@ import controller.ImageControllerImpl;
  */
 public class ImageProgram {
 
+
   /**
-   * Run the Image Processing Program in the console.
-   * Script inputs are allowed.
+   * Run the Image Processing Program in the console. Script inputs are allowed.
    *
    * @param args arguments
    */
   public static void main(String[] args) {
+    boolean text = false;
     Readable in = new InputStreamReader(System.in);
+
     for (int i = 0; i < args.length; i++) {
-      if (args[i].equalsIgnoreCase("-file")
-          || args[i].equalsIgnoreCase("-f")) {
-        try {
-          File f = new File(args[i + 1]);
-          if (f.exists() && f.isFile()) {
-            in = new InputStreamReader(new FileInputStream(f));
+      String s = args[i].toLowerCase(Locale.ROOT);
+
+      switch (s) {
+        case "-file":
+        case "-f":
+          //text = true;
+          if (i + 1 < args.length) {
+            in = setinput(args[i + 1]);
+            if (in == null) {
+              System.out.printf("could not find file: %S%nProgram will exit%n", args[i + 1]);
+              return;
+            }
             i++;
+          } else {
+            System.out.print("file argument must be followed by a filepath" + System.lineSeparator());
           }
-        } catch (FileNotFoundException e) {
-          System.out.append(e.getMessage());
+
+        case "-text":
+        case "-t":
+          text = true;
+          break;
+        default:
+          System.out.printf("unrecognized command: %S%nProgram will exit%n", args[i]);
           return;
-        }
+
       }
+
     }
-    ImageController c = new ImageControllerImpl(in, System.out);
-    c.run();
+    startProgram(in, text);
+
+  }
+
+  private static void startProgram(Readable in, boolean text) {
+    if (text) {
+      new ImageControllerImpl(in, System.out).run();
+      return;
+    }
+    ImageView frame = new ImageViewFrame();
+    new ImageControllerView(frame).run();
+  }
+
+  private static InputStreamReader setinput(String arg) {
+    try {
+      File f = new File(arg);
+      if (f.exists() && f.isFile()) {
+        InputStreamReader in = new InputStreamReader(new FileInputStream(f));
+        return in;
+      }
+    } catch (FileNotFoundException e) {
+      System.out.append(e.getMessage());
+      return null;
+    }
+    return null;
   }
 }
